c83f64e2f2b66e41f603d2f5965c15b03ccf8e94
Christine Gilbert <istinecray@gmail.com>
2020-12-09 22:51:31 -0500

    Add connection to faunadb

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import faunadb from "faunadb";
+const { query } = faunadb;
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET,
+});
+
+export default async (_, response) => {
+  response.setHeader("Content-Type", "application/json");
+
+  try {
+    const recipes = await client.query(
+      query.Map(
+        query.Paginate(query.Documents(query.Collection("recipes"))),
+        query.Lambda((recipe) => query.Get(recipe))
+      )
+    );
+
+    response.statusCode = 200;
+    response.send({
+      recipes,
+    });
+  } catch (error) {
+    response.statusCode = 500;
+    response.send({
+      error,
+    });
+  }
+};
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,42 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Recappie</title>
+        <meta name="mobile-web-app-capable" content="yes" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Recappie!
-        </h1>
+        <h1 className={styles.title}>Welcome to Recappie!</h1>
 
         <p className={styles.description}>
           An app for collecting family recipes.
         </p>
+
+        <p>{JSON.stringify(props.recipes.data.map(({ data }) => data))}</p>
       </main>
 
       <footer className={styles.footer}>
-          &copy; {new Date().getFullYear()}
+        &copy; {new Date().getFullYear()}
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const url = [process.env.BASE_URL, "api", "hello"].join("/");
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data
+    ? {
+        props: data.recipes,
+      }
+    : {
+        notFound: true,
+      };
 }
